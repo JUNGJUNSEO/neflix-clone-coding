45e3ac3812c3ee6f0a9fb2424546bbd47e07dd12
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-08-17 17:40:45 +0900

    Update Home.tsx

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import styled from "styled-components";
-import { getMovie, IGetMoviesResult } from "../api"
+import { getMovie, getupcomingMovie, getTopMovie, IGetMoviesResult } from "../api"
 import { makeImagePath } from "../utils";
 import Slider from "../components/Slider"
 
@@ -26,31 +26,43 @@ const Banner = styled.div<{ bgPhoto: string }>`
 `;
 
 const Title = styled.h2`
-  font-size: 68px;
-  margin-bottom: 20px; ;
+  font-size: 50px;
+  font-weight: 600;
+  margin-bottom: 20px;
 `;
 
 const Overview = styled.p`
-  font-size: 30px;
+  font-size: 21px;
   width: 50%;
 `;
 
+const Movies = styled.div`
+  display:grid;
+  grid-template-rows: repeat(3, 1fr);
+`;
 
 function Home() {
-    const {data, isLoading} = useQuery<IGetMoviesResult>(["movie", "nowPlaying"], getMovie)
+    const {data :nowPlaying, isLoading: nowPlayingLoading} = useQuery<IGetMoviesResult>(["movie", "nowPlaying"], getMovie)
+    const {data :upcoming, isLoading: upcomingLoading} = useQuery<IGetMoviesResult>(["movie", "Popular"], getupcomingMovie)
+    const {data :TopRated, isLoading: TopRatedLoading} = useQuery<IGetMoviesResult>(["movie", "TopRated"], getTopMovie)
     return (
         <Wrapper>
-            {isLoading ? (
+            {nowPlayingLoading ? (
                 <Loader>Loading...</Loader>
             ):(
                 <>
                   <Banner
-                    bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                    bgPhoto={makeImagePath(nowPlaying?.results[0].backdrop_path || "")}
                   >
-                    <Title>{data?.results[0].title}</Title>
-                    <Overview>{data?.results[0].overview}</Overview>
-                  </Banner>   
-                  <Slider/>
+                    <Title>{nowPlaying?.results[0].title}</Title>
+                    <Overview>{nowPlaying?.results[0].overview}</Overview>
+                  </Banner>
+                  <Movies>
+                    <Slider data={nowPlaying} id="nowPlaying"/>
+                    <Slider data={upcoming} id="upcoming"/>
+                    <Slider data={TopRated} id="topRated"/>
+                  </Movies>
+                  
                   
                   
                 </>
